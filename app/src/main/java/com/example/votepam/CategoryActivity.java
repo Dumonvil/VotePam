7c85041933e64e1e1d29f70c1966f57cb4ec0ef5
Stephane Etienne <stephaneetienne70@gmail.com>
2019-09-21 19:09:11 -0400

    Detail user

--- app/src/main/java/com/example/votepam/CategoryActivity.java
+++ app/src/main/java/com/example/votepam/CategoryActivity.java
@@ -1,14 +1,94 @@
 package com.example.votepam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
 
 public class CategoryActivity extends AppCompatActivity {
+    CardView card1,card2,card3;
+    Toolbar toolbar;
+    TextView tfirstname,tname;
+    ImageView ivuser1,ivBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        card1 = findViewById(R.id.president);
+        card2 = findViewById(R.id.senator);
+        card3 = findViewById(R.id.depite);
+        ivuser1 = findViewById(R.id.ivuser);
+        ivBack = findViewById(R.id.ivback);
+        tname = findViewById(R.id.name);
+        tfirstname = findViewById(R.id.firstname);
+
+
+        ParseUser user = ParseUser.getCurrentUser();
+
+        ParseFile img = user.getParseFile("profileImage");
+        try {
+            Bitmap bitmap = BitmapFactory.decodeStream(img.getDataStream());
+            ivuser1.setImageBitmap(bitmap);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        tfirstname.setText(user.getString("firstname"));
+        tname.setText(user.getString("name"));
+
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CategoryActivity.this,DetailUserActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CategoryActivity.this,ShowPresActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CategoryActivity.this,ShowSenActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        card3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CategoryActivity.this,ShowDepActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/votepam/ShowDepActivity.java
+++ app/src/main/java/com/example/votepam/ShowDepActivity.java
@@ -8,39 +8,31 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
 
-public class DetailUserActivity extends AppCompatActivity {
+public class ShowDepActivity extends AppCompatActivity {
     Toolbar toolbar;
-    Button btnVote;
-    TextView tfirstname,tname,tdate,tlieu,tname1,tfirstname1;
-    ImageView ivuser1, imageView,ivBack;
+    TextView tfirstname,tname;
+    ImageView ivuser1,ivBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_user);
+        setContentView(R.layout.activity_show_dep);
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        btnVote = findViewById(R.id.btn_vote);
         ivuser1 = findViewById(R.id.ivuser);
         ivBack = findViewById(R.id.ivback);
-        imageView = findViewById(R.id.imageview);
         tname = findViewById(R.id.name);
         tfirstname = findViewById(R.id.firstname);
-        tdate = findViewById(R.id.tdate1);
-        tlieu = findViewById(R.id.tlieu1);
-        tname1 = findViewById(R.id.tnom1);
-        tfirstname1 = findViewById(R.id.tprenom1);
+
 
         ParseUser user = ParseUser.getCurrentUser();
 
@@ -48,33 +40,20 @@ public class DetailUserActivity extends AppCompatActivity {
         try {
             Bitmap bitmap = BitmapFactory.decodeStream(img.getDataStream());
             ivuser1.setImageBitmap(bitmap);
-            imageView.setImageBitmap(bitmap);
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
-        tfirstname1.setText(user.getString("firstname"));
-        tname1.setText(user.getString("name"));
         tfirstname.setText(user.getString("firstname"));
         tname.setText(user.getString("name"));
-        tlieu.setText(user.getString("birthplace"));
-        //tdate.setText((CharSequence) user.getDate("birthdate"));
-
-        btnVote.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(DetailUserActivity.this,CategoryActivity.class);
-                startActivity(intent);
-
-            }
-        });
 
         ivBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(ShowDepActivity.this,CategoryActivity.class);
+                startActivity(intent);
                 finish();
             }
         });
-
     }
 }
